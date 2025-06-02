ba042b59b8246afc215b432d9e4a5de68a966596
pedro-rennil <phrss@cesar.school>
2025-06-01 21:00:55 -0300

    att logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,7 @@ function Header() {
   return (
     <header className="header">
       <div className="logo-box">
-            <img src="/logo.png" alt="Logo Fala Recife" className="logo-img-header" />
+            <img src="/FalaRecife.png" alt="Logo Fala Recife" className="logo-img-header" />
       </div>
       <nav className="nav">
         <Link to="/">Início</Link>
